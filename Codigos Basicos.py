067de8ee82738fcbd06acbcbbd225774e76fa8fe
Jorge <chuyirox@gmail.com>
2025-10-20 00:59:27 -0500

    nada

---
+++ Codigos Basicos.py
@@ -0,0 +1,23 @@
+
+
+#Indexacion con datos compuestos
+nombre_usuario = input('Ingrese su nombre:')
+print('la ultima letra de su nombre es:', nombre_usuario[-1]) #imprime la ultima letra del nombre ingresado
+
+
+#slicing
+nombre_usuario2 = 'Jorge Goyburu'
+print(nombre_usuario2[0:-1]) #De caracter 0 hasta 1 antes del -1, osea no se imprime el ultimo caracter
+print(len(nombre_usuario2))
+
+# uso de "in"
+x = 'hola mundo'
+print('m'in x)  # true
+print('z'in x)  # false
+
+
+#
+
+
+
+
