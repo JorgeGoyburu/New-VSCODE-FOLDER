8953f9b39ec8520772db7cd54c78b26c93d70135
JorgeGoyburu <chuyirox@gmail.com>
2026-04-19 20:15:14 -0500

    nuevo

--- PROJECTS/nuevo.java
+++ PROJECTS/nuevo.java
@@ -1,6 +1,12 @@
 package PROJECTS;
 public class nuevo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo"); 
+        System.out.println("Hola Mundo");
+        int codigo = 32;
+        System.out.println(codigo);
+        System.out.println("32");
+        double pi = 3.14;
+        System.out.println(pi);
+
     }
 }
